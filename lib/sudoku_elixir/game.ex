98d967d72bb8dd4438e2a3dff391e44bc0be351d
Gustavo Puglia <gustavo.puglia@alphasights.com>
2017-11-09 14:00:31 +0000

    Game spike

---
+++ lib/sudoku_elixir/game.ex
@@ -0,0 +1,15 @@
+defmodule SudokuElixir.Game do
+  def solve(board) do
+    # if Board.valid?(board) do
+    #   for move <- Enum.to_list(1..board.board_size) do
+    #     board
+    #     |> move(move, cell)
+    #     |> solve
+    #   end
+    # end
+  end
+
+  defp move(board, move, cell) do
+
+  end
+end
